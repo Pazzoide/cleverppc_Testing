3797b9f130748afb1a681d6cef4ce92b37c5b5eb
BURHAN <Brhn.Sngl.95@outlook.com>
2023-09-22 21:24:40 +0300

    ✨Project setup and basic structure✨

--- src/test/java/Pages/Patent.java
+++ src/test/java/Pages/Patent.java
@@ -6,10 +6,12 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.time.Duration;
+import java.util.List;
 
 public class Patent {
     public WebDriverWait wait = new WebDriverWait(G_W_D.getDriver(), Duration.ofSeconds(20));
@@ -43,5 +45,41 @@ public class Patent {
         scrollToElement(element);
         JavascriptExecutor js = (JavascriptExecutor) G_W_D.getDriver();
         js.executeScript("arguments[0].click();", element);
+    }
+
+    public void myHover(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        scrollToElement(element);
+        Actions actions = new Actions(G_W_D.getDriver());
+        actions.moveToElement(element).build().perform();
+    }
+
+    public void mySelect(WebElement element, String text) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        Select dMenu = new Select(element);
+        dMenu.selectByVisibleText(text);
+    }
+
+    public static void Wait(int sn) {
+        try {
+            Thread.sleep(sn * 1000L);
+        } catch (InterruptedException Ex) {
+            throw new RuntimeException(Ex);
+        }
+    }
+
+    public static int RandomGenerator(int Limit) {
+        return (int) (Math.random() * Limit);
+    }
+
+    public static boolean ListContainsString(List<WebElement> List, String Aranan) {
+        boolean Bulundu = false;
+        for (WebElement X : List) {
+            if (X.getText().toLowerCase().equals(Aranan.toLowerCase())) {
+                Bulundu = true;
+                break;
+            }
+        }
+        return Bulundu;
     }
 }
